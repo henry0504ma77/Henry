155c6223994616eac60cdac999524312bca78c16
sapien1220 <sapien1220@gmail.com>
2018-01-02 16:56:59 +0800

    start

---
+++ app/src/main/java/com/example/user/fourplus/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.fourplus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+        
+    }
+}
